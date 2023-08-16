e6fc9315bc508b1bacfc883092e764dd139bd63d
Hígor Pinheiro Costa <higorcosta2001@outlook.com>
2023-08-16 15:29:35 -0300

    middleware

---
+++ middleware/AdminAuth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const secret = '!@#_word)(*AbcKj';
+
+
+module.exports = function(req,res,next){
+    const authToken = req.headers['authorization'];
+
+
+    if (!authToken) {
+        return res.status(401).json({ error: 'Unauthorized: Token not provided' });
+    }
+    
+    const token = authToken.split(' ')[1]; // O token JWT após 'Bearer '
+    
+
+    try {
+      const decoded = jwt.verify(token, secret);
+        if(decoded.role == 1){
+            next();
+        }else{
+            return res.status(401).json({ error: 'Sem autorização' });
+        }
+
+    } catch (error) {
+      return res.status(401).json({ error: 'Unauthorized: Invalid token' });
+    }
+}
